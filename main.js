da23a05f2ba8640f81c58e096adf63c0615f953f
Guladishavadze9 <157788843+Guladishavadze9@users.noreply.github.com>
2024-05-01 10:18:00 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let country = ["საქართველო", "არგენტინა", "ესპანეთი", "ურუგუაი", "პორტუგალია"]
+let num = [3545454, 8584684, 54489748, 65598897, 884787499]
+document.write(country[0] + num[0] + "<br/>" );
+document.write(country[1] + num[1] + "<br/>" );
+document.write(country[2] + num[2] + "<br/>" );
+document.write(country[3] + num[3] + "<br/>" );
+document.write(country[4] + num[4] + "<br/>" );
